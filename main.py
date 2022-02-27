45e0609813bcae8e66683307aca80d0254ebc4b4
stephanie <ggh>
2022-02-27 23:57:47 +0100

    Done

--- main.py
+++ main.py
@@ -18,20 +18,26 @@ t.colormode(255)
 color_list = [(202, 164, 110), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
 print(color_list)
 def random_color():
-    ok = random.choice(rgb_colors)
+    ok = random.choice(color_list)
     r = ok[0]
     g = ok[1]
     b = ok[2]
     colors = (r, g, b)
     # print(colors)
     return colors
-# set_heading = lily.setheading(10)
-# for i in range(12):
-#     lily.color(random_color())
-#
-#     # lily.forward(20)
-#     lily.dot(2)
-#     current_heading = set_heading + 10
+lily.penup()
+y = -200
+for i in range(10):
+    lily.sety(y)
+    lily.setx(-300)
+    for i in range(10):
+        lily.pendown()
+        lily.dot(20,random_color())
+        lily.penup()
+        lily.forward(50)
+
+    y += 50
+
 
 
 screen = t.Screen()
